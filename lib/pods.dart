5f51493baa4194500f3cd6b8ae2a139ef8b6120f
hammadpervez <hammadpervez6@gmail.com>
2021-11-17 16:34:33 +0500

    added selection , fixing  ontapping

--- lib/pods.dart
+++ lib/pods.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:whatsapp_clone/controllers/message_controller.dart';
 import 'package:whatsapp_clone/controllers/theme_controller.dart';
 
 final themeController = StateNotifierProvider<ThemeController, ThemeMode>((ref) => ThemeController());
+final messageController = ChangeNotifierProvider((ref) => MessageController());
--- lib/views/chat_view/components/chat_bubble_widget.dart
+++ lib/views/chat_view/components/chat_bubble_widget.dart
@@ -1,68 +1,80 @@
 import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:whatsapp_clone/models/message_model/message_model.dart';
+import 'package:whatsapp_clone/pods.dart';
 import 'package:whatsapp_clone/res/colors.dart';
 import 'package:whatsapp_clone/res/extensions.dart';
 
-class ChatBubbleWidget extends StatelessWidget {
+class ChatBubbleWidget extends ConsumerWidget {
   const ChatBubbleWidget({
     Key? key,
     required this.chat,
   }) : super(key: key);
-
   final MessageModel chat;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    var controller = ref.watch(messageController);
     return Container(
+      margin: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+      foregroundDecoration: controller.selectedItems.contains(chat)
+          ? BoxDecoration(color: Colors.blue.withOpacity(.4))
+          : null,
       constraints: BoxConstraints(
-        maxWidth: context.width * .85,
-        minWidth: context.width * .4,
-      ),
+          maxWidth: context.width * .85, minWidth: context.width * .4),
       alignment: !chat.isSent ? Alignment.centerLeft : Alignment.centerRight,
-      child: Stack(
-        children: [
-          Bubble(
-            padding: const BubbleEdges.all(0),
-            margin: const BubbleEdges.symmetric(horizontal: 8, vertical: 4),
-            nip: !chat.isSent ? BubbleNip.leftTop : BubbleNip.rightTop,
-            color: !chat.isSent ? kWhiteColor : kChatColor,
-            child: Material(
-              type: MaterialType.transparency,
-              child: InkWell(
-                onLongPress: () {},
-                child: Container(
-                  padding: const EdgeInsets.fromLTRB(8, 6, 8, 6),
-                  child: Stack(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
-                        child: Text.rich(
-                          TextSpan(
-                            children: [
-                              TextSpan(text: chat.message),
-                              const WidgetSpan(child: const SizedBox(width: 8)),
-                              //PlaceHolder
-                              WidgetSpan(
-                                child: _buildBubbleCaptionText(
-                                    context, chat, Colors.transparent),
-                              ),
-                            ],
-                          ),
+      child: GestureDetector(
+        onTap: () {
+          print("Controller ${controller.selectedItems}");
+          if (controller.selectedItems.contains(chat))
+            controller.unSelectItem(chat);
+          else if (controller.selectedItems.isNotEmpty)
+            controller.selectItem(chat);
+        },
+        child: Bubble(
+          padding: const BubbleEdges.all(0),
+          margin: const BubbleEdges.symmetric(horizontal: 8),
+          nip: !chat.isSent ? BubbleNip.leftTop : BubbleNip.rightTop,
+          color: !chat.isSent ? kWhiteColor : kChatColor,
+          child: Material(
+            type: MaterialType.transparency,
+            child: InkWell(
+              onLongPress: () {
+                controller.updateItemSelected();
+                controller.selectItem(chat);
+              },
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 4),
+                      child: Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(text: chat.message),
+                            const WidgetSpan(child: const SizedBox(width: 8)),
+                            //PlaceHolder
+                            WidgetSpan(
+                              child: _buildBubbleCaptionText(
+                                  context, chat, Colors.transparent),
+                            ),
+                          ],
                         ),
                       ),
-                      Positioned(
-                        right: 0,
-                        bottom: 0,
-                        child: _buildBubbleCaptionText(context, chat),
-                      )
-                    ],
-                  ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      child: _buildBubbleCaptionText(context, chat),
+                    )
+                  ],
                 ),
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
