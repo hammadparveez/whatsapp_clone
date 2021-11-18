78917a97577810c49c1e6ab001f6bc6267a7c0bd
hammadpervez <hammadpervez6@gmail.com>
2021-11-18 19:17:18 +0500

    fixed emoji picker

--- lib/views/chat_view/components/message_type_container.dart
+++ lib/views/chat_view/components/message_type_container.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 import 'dart:ui';
 
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:whatsapp_clone/config/without_glow_scroll_configuraiton.dart';
 import 'package:whatsapp_clone/res/colors.dart';
 import 'package:whatsapp_clone/res/extensions.dart';
@@ -16,7 +18,7 @@ class MessageTypeContainer extends StatefulWidget {
 
 class _MessageTypeContainerState extends State<MessageTypeContainer> {
   late TextEditingController _controller;
-
+  bool _isEmojiVisible = false, _hadFocus = false;
   @override
   void initState() {
     super.initState();
@@ -29,86 +31,137 @@ class _MessageTypeContainerState extends State<MessageTypeContainer> {
         setState(() {});
       }
     });
+
+    Future.microtask(() {
+      FocusScope.of(context).addListener(() {
+        var focusScope = FocusScope.of(context);
+        if (focusScope.hasFocus && _isEmojiVisible) {
+          setState(() {
+            _hadFocus = true;
+            focusScope.unfocus();
+          });
+        }
+        print("Has Focus ${FocusScope.of(context).hasFocus}");
+      });
+    });
+  }
+
+  _onShowEmojis() {
+    var focusScope = FocusScope.of(context);
+
+    setState(() {
+      if (focusScope.hasFocus) {
+        _hadFocus = true;
+        focusScope.unfocus();
+      } else {
+        if (_hadFocus) {
+          _hadFocus = false;
+          focusScope.requestFocus();
+        }
+      }
+      _isEmojiVisible = !_isEmojiVisible;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //alignment: Alignment.bottomCenter,
+    final maxHeight = (context.height - kToolbarHeight);
+    return AnimatedContainer(
+      duration: const Duration(seconds: 1),
       padding: const EdgeInsets.fromLTRB(8, 4, 8, 8),
       color: kCreamColor,
-      constraints:
-          BoxConstraints(maxHeight: (context.height - kToolbarHeight) / 2.5),
-      child: Stack(children: [
-        Row(
-          children: [
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.only(bottom: 4),
-                decoration: BoxDecoration(
-                  color: kWhiteColor,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                        bottom: 0,
-                        child: _buildMessageIcon(
-                            Icons.emoji_emotions_outlined, () {})),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
+      constraints: BoxConstraints(
+          maxHeight: _isEmojiVisible ? maxHeight - 50 : maxHeight / 2),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Stack(children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    decoration: BoxDecoration(
+                      color: kWhiteColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Stack(
                       children: [
-                        spacer(),
-                        Expanded(
-                          child: ScrollConfiguration(
-                            behavior: NoGlowScrollBehavior(),
-                            child: Scrollbar(
-                              child: TextField(
-                                controller: _controller,
-                                decoration: const InputDecoration(
-                                  contentPadding:
-                                      EdgeInsets.fromLTRB(0, 8, 0, 8),
-                                  isCollapsed: true,
-                                  isDense: true,
-                                  hintText: 'Message',
-                                  border: InputBorder.none,
+                        Positioned(
+                            bottom: 0,
+                            child: _buildMessageIcon(
+                                Icons.emoji_emotions_outlined, _onShowEmojis)),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            spacer(),
+                            Expanded(
+                              child: ScrollConfiguration(
+                                behavior: NoGlowScrollBehavior(),
+                                child: Scrollbar(
+                                  child: TextField(
+                                    onTap: () {
+                                      if (_isEmojiVisible) {
+                                        setState(() {
+                                          _isEmojiVisible = false;
+                                          FocusScope.of(context).requestFocus();
+                                        });
+                                      }
+                                    },
+                                    controller: _controller,
+                                    decoration: const InputDecoration(
+                                      contentPadding:
+                                          EdgeInsets.fromLTRB(0, 8, 0, 8),
+                                      isCollapsed: true,
+                                      isDense: true,
+                                      hintText: 'Message',
+                                      border: InputBorder.none,
+                                    ),
+                                    maxLines: null,
+                                  ),
                                 ),
-                                maxLines: null,
                               ),
                             ),
-                          ),
+                            //Row of Camera and Attachfile IconButton
+                            spacer(),
+                          ],
                         ),
-                        //Row of Camera and Attachfile IconButton
-                        spacer(),
+                        endedButtons(),
                       ],
                     ),
-                    endedButtons(),
-                  ],
+                  ),
+                ),
+                const SizedBox(
+                    width: kMinInteractiveDimension + 8,
+                    height: kMinInteractiveDimension * .9),
+              ],
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Material(
+                color: context.primaryColor,
+                shape: const CircleBorder(),
+                clipBehavior: Clip.hardEdge,
+                child: IconButton(
+                  color: kWhiteColor,
+                  constraints: const BoxConstraints(
+                      minHeight: kMinInteractiveDimension * .8),
+                  icon: _swtichingMicIcon(),
+                  onPressed: () {},
                 ),
               ),
             ),
-            const SizedBox(
-                width: kMinInteractiveDimension + 8,
-                height: kMinInteractiveDimension * .9),
-          ],
-        ),
-        Positioned(
-          bottom: 0,
-          right: 0,
-          child: Material(
-            color: context.primaryColor,
-            shape: const CircleBorder(),
-            clipBehavior: Clip.hardEdge,
-            child: IconButton(
-              color: kWhiteColor,
-              constraints: const BoxConstraints(
-                  minHeight: kMinInteractiveDimension * .8),
-              icon: _swtichingMicIcon(),
-              onPressed: () {},
-            ),
-          ),
-        ),
-      ]),
+          ]),
+          !_isEmojiVisible
+              ? const SizedBox()
+              : SizedBox(
+                  height: 200,
+                  child: EmojiPicker(onEmojiSelected: (category, emoji) {
+                    _controller.text += emoji.emoji;
+                  })),
+        ],
+      ),
     );
   }
 
